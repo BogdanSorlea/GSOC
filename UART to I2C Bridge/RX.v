00450ce11ad6fb4e9a55c7e4854b62effaa92f9d
vladniculescu <nicu_vlad10@hotmail.com>
2017-07-28 00:02:47 +0300

    GSOC 2nd ev 3

--- UART to I2C Bridge/RX.v
+++ UART to I2C Bridge/RX.v
@@ -1,24 +1,8 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/26/2016 02:30:57 PM
-// Design Name: 
-// Module Name: RX
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+//Author: Niculescu Vlad
+//Copyright (C)2017
+//Licensed under CERN OHL v1.2
 
 module RX(input in,
           input clock,
@@ -26,7 +10,7 @@ module RX(input in,
           output reg d_avail);
           
 reg [2:0] state=0;
-parameter P=10416;
+parameter P=10416;   //prescaler: P = SYSCLK/BaudRate
 integer i;
 integer bit_pos;
 parameter s0=3'b000,
@@ -35,57 +19,57 @@ parameter s0=3'b000,
           s3=3'b011,
           s4=3'b100;
 
-always @(posedge clock)
-begin
-case(state)
-s0:begin    
-   d_avail<=1'b0;
-   i=0;                   //init
-   bit_pos=0;
-   if(in==1'b0) begin     //start bit detected
+always @(posedge clock) begin
+    case(state)
+        s0:begin    
+            d_avail<=1'b0;
+            i=0;                   
+            bit_pos=0;
+            if(in==1'b0) begin     
                 state<=s1; 
                 i=1;
+            end
+        end
+           
+        s1:begin    
+            if(i==P-1) begin
+               state<=s2;
+               i=0;
+               out <= 0;
+            end
+            else 
+                i=i+1;
+            end
+           
+        s2:begin
+            if(i == P) 
+                if(bit_pos==7) begin
+                    state<=s3;
+                    i=0;
                 end
-   end
-   
-s1:begin    
-   if(i==P-1) begin
-              state<=s2;
-              i=0;
-              out<=8'b00000000;
-              end
-    else i=i+1;
-    end
-   
-s2:begin
-   if(i==P) if(bit_pos==7) begin
-                           state<=s3;
-                           i=0;
-                           end
-            else begin 
-                 bit_pos=bit_pos+1;
-                 i=0;
-                 end 
-    else begin
-         if(i==P/2) out[bit_pos]<=in;
-         i=i+1;
-         end         
-   end
-   
-s3:begin
-   if(i==P) begin
-              state<=s4;
-              i=0;
-              end
-   else i=i+1;
-   end
-
-s4:begin
-   d_avail<=1'b1;
-   if(i==2) state<=s0;
-   else i=i+1;
-   end
-   
-endcase
+                else begin 
+                    bit_pos=bit_pos+1;
+                    i=0;
+                end 
+            else begin
+                 if(i==P/2) out[bit_pos]<=in;
+                 i=i+1;
+            end         
+            end
+           
+        s3:begin
+            if(i==P) begin
+                state<=s4;
+                i=0;
+            end
+            else 
+                i=i+1;
+            end
+        
+        s4:begin
+               d_avail<=1'b1;
+               state<=s0; 
+        end           
+    endcase
 end
 endmodule
